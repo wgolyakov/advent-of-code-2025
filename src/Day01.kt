9e1b4d72a674c655d684f774d57c53fbaa0c865d
wgolyakov <wolodia@narod.ru>
2025-12-01 08:51:48 +0300

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,39 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+		var dial = 50
+		var zeroCount = 0
+		for (line in input) {
+			val d = line[0]
+			val n = line.drop(1).toInt()
+			dial = if (d == 'L') (dial + 100 - (n % 100)) % 100 else (dial + n) % 100
+			if (dial == 0) zeroCount++
+		}
+		return zeroCount
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+		var dial = 50
+		var zeroCount = 0
+		for (line in input) {
+			val d = line[0]
+			val n = line.drop(1).toInt()
+			if (d == 'L') {
+				zeroCount += n / 100
+				if (dial > 0 && dial <= n % 100) zeroCount++
+				dial = (dial + 100 - (n % 100)) % 100
+			} else {
+				zeroCount += (dial + n) / 100
+				dial = (dial + n) % 100
+			}
+		}
+		return zeroCount
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+	check(part2(testInput) == 6)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    part1(input).println() // 1040
+    part2(input).println() // 6027
 }
