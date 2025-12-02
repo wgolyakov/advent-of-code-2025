240e4e9b11f64da7e0d7e655ddd785f754b64356
wgolyakov <wolodia@narod.ru>
2025-12-02 09:34:22 +0300

    Template

--- .idea/fileTemplates/Advent of Code.kt
+++ .idea/fileTemplates/Advent of Code.kt
@@ -1,16 +1,18 @@
 #set( $Code = "bar" )
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+	fun part1(input: List<String>): Int {
+		return 1
+	}
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+	fun part2(input: List<String>): Int {
+		return 1
+	}
 
-    check(part1(listOf("...")) == 1)
+	val testInput = readInput("Day${Day}_test")
+	check(part1(testInput) == 1)
+	check(part2(testInput) == 1)
 
-    val input = readInput("Day$Day")
-    part1(input).println()
-    part2(input).println()
+	val input = readInput("Day$Day")
+	part1(input).println()
+	part2(input).println()
 }
